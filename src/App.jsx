ccf7127a491b4680f31e65f69acfe16601ed142b
Hedita <hedi.takrami@gmail.com>
2023-02-15 00:36:56 +0330

    config and add src files

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+import SearchInput from "./SearchInput";
+
+const App = () => {
+  return <SearchInput />
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ src/SearchInput.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Contacts from "./ContactListData,";
+
+function SearchInput () {
+  
+}
+
+export default SearchInput;
