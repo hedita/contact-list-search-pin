b18b5a8445cbb58679391206e3b5bdf7ec0077b2
Hedita <hedi.takrami@gmail.com>
2023-02-16 21:03:00 +0330

    add search component

--- src/SearchInput.jsx
+++ src/SearchInput.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Contacts from "./ContactListData,";
 
-function SearchInput () {
-  
+function SearchInput() {
+  const [contact, setContact] = useState("");
+  return (
+    <>
+    <input className="contact-input" placeholder="Contacts"/>
+     {Contacts.map(({ id, name, email, isPined }) => {
+    return (
+      <div className="contact" key={id}>
+        <p className="contact-name">{name}</p>
+        <p className="contact-email">{email}</p>
+      </div>
+    );
+  })
+}
+    </>
+  )
 }
 
 export default SearchInput;
