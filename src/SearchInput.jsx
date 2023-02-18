da6b61f89ed62ff8ab18d9211c99b6378d90dec1
Hedita <hedi.takrami@gmail.com>
2023-02-18 22:19:02 +0330

    handle filter contact and add pin icon

--- src/SearchInput.jsx
+++ src/SearchInput.jsx
@@ -1,22 +1,57 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import Contacts from "./ContactListData,";
 
 function SearchInput() {
-  const [contact, setContact] = useState("");
+  const [foundContacts, setFoundContacts] = useState(Contacts);
+
+  Contacts.sort(function (a, b) {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name > b.name) {
+      return 1;
+    }
+    return 0;
+  });
+
+  const filter = (e) => {
+    const keyword = e.target.value;
+
+    if (keyword == "") {
+      setFoundContacts(Contacts);
+    } else {
+      const results = Contacts.filter(({ name, email }) => {
+        return (
+          name.toLowerCase().startsWith(keyword.toLowerCase()) ||
+          email.toLowerCase().startsWith(keyword.toLowerCase())
+        );
+      });
+      setFoundContacts(results);
+    }
+  };
+
   return (
     <>
-    <input className="contact-input" placeholder="Contacts"/>
-     {Contacts.map(({ id, name, email, isPined }) => {
-    return (
-      <div className="contact" key={id}>
-        <p className="contact-name">{name}</p>
-        <p className="contact-email">{email}</p>
-      </div>
-    );
-  })
-}
+      <input
+        type="search"
+        onChange={filter}
+        className="contact-input"
+        placeholder="Contacts"
+      />
+
+      {foundContacts.length > 0 ? (
+        foundContacts.map(({ id, name, email }) => (
+          <div key={id} className="contact">
+            <p className="contact-name">{name}</p>
+            <p className="contact-email">{email}</p>
+            <i className="fa-solid fa-thumbtack pin-icon"></i>
+          </div>
+        ))
+      ) : (
+        <h1>NOT FOUND!</h1>
+      )}
     </>
-  )
+  );
 }
 
 export default SearchInput;
